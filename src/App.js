27bb2c9b9035586584a48cdcb684ccb151743f7f
timlaurijs <timlaurijs@gmail.com>
2020-08-24 14:19:54 +0200

    added Switch/Route to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
-import Homepage from "./pages/Homepage";
+import React from "react"
+import { Switch, Route } from "react-router-dom"
+
+import Homepage from "./pages/Homepage"
 
 function App() {
   return (
-    <div>
-      <Homepage />
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
