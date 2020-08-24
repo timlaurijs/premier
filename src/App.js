3b6e29ec0e920cc60cffce97b50307b8dadf21f7
timlaurijs <timlaurijs@gmail.com>
2020-08-24 16:36:53 +0200

    added useEffect with getUserWithStoredToken action

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react"
+import { Switch, Route } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
 
-import Homepage from "./pages/Homepage";
-import Dashboard from "./pages/Dashboard";
+import Homepage from "./pages/Homepage"
+import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login"
+import { getUserWithStoredToken } from "./store/user/actions"
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getUserWithStoredToken())
+  }, [dispatch])
+
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={Login} />
       </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
