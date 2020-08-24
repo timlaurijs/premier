4ab9c7af49c71cfce48100135cb0735782fef87d
grakify90 <ninavanes1990@hotmail.com>
2020-08-24 14:39:16 +0200

    made basic dashboard page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react"
-import { Switch, Route } from "react-router-dom"
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import Homepage from "./pages/Homepage"
+import Homepage from "./pages/Homepage";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
