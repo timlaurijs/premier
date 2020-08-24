836707e56b69fe5e5096364a24c3d8d1d4667435
grakify90 <ninavanes1990@hotmail.com>
2020-08-24 14:57:36 +0200

    made a basic version of navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/dashboard" component={Dashboard} />
