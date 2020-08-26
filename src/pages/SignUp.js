4c6d2eb8a3d4b12b224940b4997ec221329a7cad
mayallzObject <mayallzobject@gmail.com>
2020-08-26 12:21:28 +0200

    basic image upload on signup, set with firebase

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,15 +3,69 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectToken } from "../store/user/selectors";
 import { useHistory } from "react-router-dom";
 import { signUp } from "../store/user/actions";
-import { Box } from "@material-ui/core";
+import { Box, Button, IconButton, makeStyles } from "@material-ui/core";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
+
+// FireBase
+import { storage } from "../Firebase";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  input: {
+    display: "none",
+  },
+}));
 
 const SignUp = () => {
+  const classes = useStyles();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
   const token = useSelector(selectToken);
   const history = useHistory();
+  const [image, setImage] = useState(null);
+  const [url, setUrl] = useState("");
+  const [progress, setProgress] = useState(0);
+
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  };
+
+  const handleUpload = () => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            setUrl(url);
+          });
+      }
+    );
+  };
+
+  console.log("image: ", image);
+
+  console.log("image", image);
 
   useEffect(() => {
     if (token !== null) {
@@ -50,9 +104,16 @@ const SignUp = () => {
             placeholder="password"
             name="password"
           ></input>
-          <button type="submit"> Submit</button>
+
+          <Button type="submit"> Submit</Button>
         </form>
       </div>
+      <input type="file" onChange={handleChange} />
+      <button onClick={handleUpload}>Upload</button>
+      <br />
+
+      <br />
+      <img src={url || "http://via.placeholder.com/300"} alt="firebaseimage" />
     </Box>
   );
 };
