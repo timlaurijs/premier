1f271cbaf09e948edad51c8821cb5fb948d5a9da
mayallzObject <mayallzobject@gmail.com>
2020-08-27 11:10:48 +0200

    home page styling, first step

--- src/components/PremierFounder.js
+++ src/components/PremierFounder.js
@@ -1,17 +1,87 @@
-import React from "react"
+import React from "react";
+
+// Mui components
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import ShareIcon from "@material-ui/icons/Share";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      maxWidth: 400,
+    },
+    media: {
+      paddingTop: "56.25%", // 16:9
+      minHeight: 400,
+    },
+    expand: {
+      transform: "rotate(0deg)",
+      marginLeft: "auto",
+      transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: "rotate(180deg)",
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  })
+);
 
 export default function PremierFounder(props) {
+  const classes = useStyles();
+
   return (
-    <div style={{ margin: "5px" }}>
-      <h2 style={{ fontSize: 25, textAlign: "center" }}>{props.name}</h2>
-      <img
-        style={{
-          width: "100%",
-        }}
-        src={props.imageUrl}
-        alt={props.name}
+    <Card className={classes.root}>
+      <CardHeader
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.name}
+        subheader={props.description}
       />
-      <p style={{ fontSize: 20, textAlign: "center" }}>{props.description}</p>
-    </div>
-  )
+
+      <CardMedia
+        className={classes.media}
+        image={props.imageUrl}
+        title={props.name}
+      />
+
+      <CardContent>
+        <Typography component="p">{props.description}</Typography>
+      </CardContent>
+
+      <CardActions disableSpacing>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+
+      <CardContent>
+        <Typography paragraph>Method:</Typography>
+
+        <Typography paragraph>
+          Heat 1/2 cup of the broth in a pot until simmering, add saffron and
+          set aside for 10 minutes.
+        </Typography>
+        <Typography paragraph></Typography>
+        <Typography paragraph></Typography>
+        <Typography>
+          Set aside off of the heat to let rest for 10 minutes, and then serve.
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
