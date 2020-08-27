6abb521fe721013cadd0496dae10dec85dba3cd0
timlaurijs <timlaurijs@gmail.com>
2020-08-27 14:32:05 +0200

    added ChangeUserData and changed buttons on Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,26 +1,58 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { useSelector } from "react-redux"
-import { Button, Box } from "@material-ui/core"
 import { Link } from "react-router-dom"
+
+//store items
 import { selectUser } from "../store/user/selectors"
 
-export default function Dashboard() {
-  const { token, name } = useSelector(selectUser)
+//components
+import ChangeUserData from "../components/ChangeUserData"
 
-  if (token) {
+//material ui
+import { Button, Box } from "@material-ui/core"
+
+export default function Dashboard() {
+  const user = useSelector(selectUser)
+  const [change, setChange] = useState(false)
+
+  if (user.token) {
     return (
       <Box mt={10} style={{ fontSize: 25, marginLeft: 20 }}>
-        <h1>Welcome, {name}!</h1>
-        <p>What do you want to do today?</p>
-        <Button variant="contained" color="primary">
-          Motivate me!
-        </Button>
-        <Button variant="contained" color="primary">
-          CS trivia quiz
-        </Button>
-        <Button variant="contained" color="primary">
-          Test my coding skills
-        </Button>
+        <h1>Welcome, {user.name}!</h1>
+        {!change ? (
+          <>
+            <p>What do you want to do today?</p>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setChange(true)}
+            >
+              Change my information
+            </Button>
+          </>
+        ) : (
+          <>
+            <p>change your information</p>
+            <ChangeUserData user={user} />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setChange(false)}
+            >
+              Back to dashboard
+            </Button>
+          </>
+        )}
+        {!change ? (
+          <>
+            <Button variant="contained" color="primary">
+              CS trivia quiz
+            </Button>
+            <Button variant="contained" color="primary">
+              Test my coding skills
+            </Button>
+          </>
+        ) : null}
       </Box>
     )
   } else {
